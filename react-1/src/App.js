d16752cbe9412f38b83ff582b118325eec9d9f73
daiit00n <mcswordyt@gmail.com>
2024-03-10 20:30:43 +0500

    react

---
+++ react-1/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Anafin Damir</h1>
+      <p>React homework 1</p>
+    </div>
+  );
+}
+
+export default App;
